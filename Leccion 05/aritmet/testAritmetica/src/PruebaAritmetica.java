a3e320eea460ed576e3a5d8c2047f94259ff6c01
Enzo Gonzalo Gramajo <112021063+chanta96@users.noreply.github.com>
2022-10-22 12:34:40 -0300

    aprendiendo java, creacion de clase y prueba de clase

---
+++ Leccion 05/aritmet/testAritmetica/src/PruebaAritmetica.java
@@ -0,0 +1,10 @@
+package aritmet;
+
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+        Aritmetica arit1 = new Aritmetica();
+        arit1.a = 15;
+        arit1.b = 5;
+        arit1.sumarAritmetica();
+    }
+}
